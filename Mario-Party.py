e2b34ba136f650af43ba801ffd6c9acf00212751
Rodger Oliver <timeline.mb@gmail.com>
2022-06-09 15:52:04 -0300

    Initial commit

---
+++ Mario-Party.py
@@ -0,0 +1,305 @@
+class No(object):
+    def __init__(self, pai=None, valor1=None, valor2=None, anterior=None, proximo=None):
+        self.pai       = pai
+        self.valor1    = valor1
+        self.valor2    = valor2
+        self.anterior  = anterior
+        self.proximo   = proximo
+
+class lista(object):
+    head = None
+    tail = None
+
+    # INSERE NO INÍCIO DA LISTA
+    def inserePrimeiro(self, v1, v2, p):
+        novo_no = No(p, v1, v2, None, None)
+        if self.head == None:
+            self.tail = novo_no
+            self.head = novo_no
+        else:
+            novo_no.proximo = self.head
+            self.head.anterior = novo_no
+            self.head = novo_no
+
+    # INSERE NO FIM DA LISTA
+    def insereUltimo(self, v1, v2, p):
+
+        novo_no = No(p, v1, v2, None, None)
+
+        if self.head is None:
+            self.head = novo_no
+        else:
+            self.tail.proximo = novo_no
+            novo_no.anterior   = self.tail
+        self.tail = novo_no
+
+    # REMOVE NO INÍCIO DA LISTA
+    def deletaPrimeiro(self):
+        if self.head is None:
+            return None
+        else:
+            no = self.head
+            self.head = self.head.proximo
+            if self.head is not None:
+                self.head.anterior = None
+            else:
+                self.tail = None
+            return no
+
+    # REMOVE NO FIM DA LISTA
+    def deletaUltimo(self):
+        if self.tail is None:
+            return None
+        else:
+            no = self.tail
+            self.tail = self.tail.anterior
+            if self.tail is not None:
+                self.tail.proximo = None
+            else:
+                self.head = None
+            return no
+
+    # RETORNA O PRIMEIRO DA LISTA
+    def primeiro(self):
+        return self.head
+
+    # RETORNA O ÚLTIMO DA LISTA
+    def ultimo(self):
+        return self.tail
+
+    # VERIFICA SE LISTA ESTÁ VAZIA
+    def vazio(self):
+        if self.head is None:
+            return True
+        else:
+            return False
+
+    # EXIBE O CONTEÚDO DA LISTA
+    def exibeLista(self):
+
+        aux = self.head
+        str = []
+        while aux != None:
+            temp = []
+            temp.append(aux.valor1)
+            temp.append(aux.valor2)
+            str.append(temp)
+            aux = aux.proximo
+
+        return str
+
+    # EXIBE O CAMINHO ENCONTRADO
+    def exibeCaminho(self):
+
+        atual = self.tail
+        caminho = []
+        while atual.pai is not None:
+            caminho.append(atual.valor1)
+            atual = atual.pai
+        caminho.append(atual.valor1)
+        caminho = caminho[::-1]
+        return caminho
+
+    # EXIBE O CAMINHO ENCONTRADO (BIDIRECIONAL)
+    def exibeCaminho1(self,valor):
+
+        atual = self.head
+        while atual.valor1 != valor:
+            atual = atual.proximo
+
+        caminho = []
+        atual = atual.pai
+        while atual.pai is not None:
+            caminho.append(atual.valor1)
+            atual = atual.pai
+        caminho.append(atual.valor1)
+        return caminho
+
+class busca(object):
+
+    # BUSCA EM AMPLITUDE
+    def amplitude(self, inicio, fim):
+
+        # manipular a FILA para a busca
+        l1 = lista()
+
+        # cópia para apresentar o caminho (somente inserção)
+        l2 = lista()
+
+        # insere ponto inicial como nó raiz da árvore
+        l1.insereUltimo(inicio,0,None)
+        l2.insereUltimo(inicio,0,None)
+
+        # controle de nós visitados
+        visitado = []
+        linha = []
+        linha.append(inicio)
+        linha.append(0)
+        visitado.append(linha)
+
+        while l1.vazio() == False:
+            # remove o primeiro da fila
+            atual = l1.deletaPrimeiro()
+            #if atual is None: break
+
+            ind = nos.index(atual.valor1)
+
+            # varre todos as conexões dentro do grafo a partir de atual
+            for i in range(len(grafo[ind])):
+
+                novo = grafo[ind][i]
+                # pressuponho que não foi visitado
+                flag = True
+
+                # controle de nós repetidos
+                for j in range(len(visitado)):
+                    if visitado[j][0]==novo:
+                        if visitado[j][1]<=(atual.valor2+1):
+                            flag = False
+                        else:
+                            visitado[j][1]=atual.valor2+1
+                        break
+
+                # se não foi visitado inclui na fila
+                if flag:
+                    l1.insereUltimo(novo, atual.valor2 + 1, atual)
+                    l2.insereUltimo(novo, atual.valor2 + 1, atual)
+
+                    # marca como visitado
+                    linha = []
+                    linha.append(novo)
+                    linha.append(atual.valor2+1)
+                    visitado.append(linha)
+
+                    # verifica se é o objetivo
+                    if novo == fim:
+                        caminho = []
+                        caminho += l2.exibeCaminho()
+                        #print("Fila:\n",l1.exibeLista())
+                        #print("\nÁrvore de busca:\n",l2.exibeLista())
+                        return caminho
+
+        return "caminho não encontrado"
+
+
+    # BUSCA EM PROFUNDIDADE
+    def profundidade(self, inicio, fim):
+
+        # manipular a FILA para a busca
+        l1 = lista()
+
+        # cópia para apresentar o caminho (somente inserção)
+        l2 = lista()
+
+        # insere ponto inicial como nó raiz da árvore
+        l1.insereUltimo(inicio,0,None)
+        l2.insereUltimo(inicio,0,None)
+
+        # controle de nós visitados
+        visitado = []
+        linha = []
+        linha.append(inicio)
+        linha.append(0)
+        visitado.append(linha)
+
+        while l1.vazio() == False:
+            # remove o primeiro da fila
+            atual = l1.deletaUltimo()
+            #if atual is None: break
+
+            ind = nos.index(atual.valor1)
+
+            # varre todos as conexões dentro do grafo a partir de atual
+            for i in range(len(grafo[ind])):
+
+                novo = grafo[ind][i]
+                # pressuponho que não foi visitado
+                flag = True
+
+                # controle de nós repetidos
+                for j in range(len(visitado)):
+                    if visitado[j][0]==novo:
+                        if visitado[j][1]<=(atual.valor2+1):
+                            flag = False
+                        else:
+                            visitado[j][1]=atual.valor2+1
+                        break
+
+                # se não foi visitado inclui na fila
+                if flag:
+                    l1.insereUltimo(novo, atual.valor2 + 1, atual)
+                    l2.insereUltimo(novo, atual.valor2 + 1, atual)
+
+                    # marca como visitado
+                    linha = []
+                    linha.append(novo)
+                    linha.append(atual.valor2+1)
+                    visitado.append(linha)
+
+                    # verifica se é o objetivo
+                    if novo == fim:
+                        caminho = []
+                        caminho += l2.exibeCaminho()
+                        #print("Fila:\n",l1.exibeLista())
+                        #print("\nÁrvore de busca:\n",l2.exibeLista())
+                        return caminho
+        return "caminho não encontrado"
+
+"""
+********************************************************************
+                     PROBLEMA: MARIO PARTY
+********************************************************************
+"""
+
+nos = [
+        "A", "B", "C", "D", "E", "F", "G", "H",
+        "I", "J", "K", "L", "M", "N", "O",
+        "P", "Q", "R", "S", "T", "U", "V",
+        "W", "X", "Y", "Z"
+        ]
+
+# ORDEM DESORDENADA
+grafo = [
+        ["C", "S", "R", "B"],
+        ["P", "U", "G", "A"],
+        ["D", "Z", "H", "A"],
+        ["S", "R", "F"],
+        ["P", "I", "X", "T"],
+        ["D", "Z", "K", "T", "X"],
+        ["B", "J", "V"],
+        ["C", "K", "Y", "N"],
+        ["E", "J", "U", "L", "M", "X"],
+        ["G", "I", "W"],
+        ["F", "H", "X", "L"],
+        ["N", "I", "K", "X"],
+        ["O", "W", "V", "I"],
+        ["Y", "L", "O", "H"],
+        ["N", "M"],
+        ["B", "Q", "E"],
+        ["R", "T", "P"],
+        ["A", "Q", "D"],
+        ["A", "Z", "C", "D"],
+        ["Q", "F", "E"],
+        ["B", "I", "E"],
+        ["G", "W", "M"],
+        ["J", "V", "M"],
+        ["E", "I", "L", "F", "K"],
+        ["N", "H"],
+        ["S", "C", "F", "D"],
+        ]
+
+# PROGRAMA PRINCIPAL
+
+sol = busca()
+caminho = []
+
+# DEFINIÇÃO DO PROBLEMA
+origem  = "A"
+destino = "O"
+
+caminho = sol.amplitude(origem,destino)
+print("\nAMPLITUDE:",caminho)
+
+caminho = sol.profundidade(origem,destino)
+print("\nPROFUNDIDADE:",caminho)
+
